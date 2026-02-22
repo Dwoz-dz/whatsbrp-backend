1a626f62a27dad4d4f85b888393606344ab1d8f0
Dwoz-dz <mohcenneddam@gmail.com>
2026-02-22 01:01:09 +0100

    chore: stop tracking dist

--- dist/ws/ws.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roomId = void 0;
-const roomId = (conversationId) => `conv:${conversationId}`;
-exports.roomId = roomId;
-//# sourceMappingURL=ws.types.js.map
